37fdb042b3da1ed2292827aba0a006f21bd362c9
Duo Zhou <duozhou2@illinois.edu>
2025-01-10 19:22:41 -0600

    Updated for PPO

--- utils.py
+++ utils.py
@@ -1,9 +1,5 @@
-import torch
 import argparse
-import numpy as np
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.distributions import Beta, Normal
 
 def build_net(layer_shape, hidden_activation, output_activation):
 	'''Build net with for loop'''
@@ -12,131 +8,6 @@ def build_net(layer_shape, hidden_activation, output_activation):
 		act = hidden_activation if j < len(layer_shape)-2 else output_activation
 		layers += [nn.Linear(layer_shape[j], layer_shape[j+1]), act()]
 	return nn.Sequential(*layers)
-
-
-class Actor(nn.Module):
-	def __init__(self, state_dim, action_dim, hid_shape, hidden_activation=nn.ReLU, output_activation=nn.ReLU):
-		super(Actor, self).__init__()
-		layers = [state_dim] + list(hid_shape)
-
-		self.a_net = build_net(layers, hidden_activation, output_activation)
-		self.mu_layer = nn.Linear(layers[-1], action_dim)
-		self.log_std_layer = nn.Linear(layers[-1], action_dim)
-
-		self.LOG_STD_MAX = 2
-		self.LOG_STD_MIN = -20
-
-	def forward(self, state, deterministic, with_logprob):
-		'''Network with Enforcing Action Bounds'''
-		net_out = self.a_net(state)
-		mu = self.mu_layer(net_out)
-		log_std = self.log_std_layer(net_out)
-		log_std = torch.clamp(log_std, self.LOG_STD_MIN, self.LOG_STD_MAX)  #总感觉这里clamp不利于学习
-		# we learn log_std rather than std, so that exp(log_std) is always > 0
-		std = torch.exp(log_std)
-		dist = Normal(mu, std)
-		u = mu if deterministic else dist.rsample()
-
-		'''↓↓↓ Enforcing Action Bounds, see Page 16 of https://arxiv.org/pdf/1812.05905.pdf ↓↓↓'''
-		a = torch.tanh(u)
-		if with_logprob:
-			# Get probability density of logp_pi_a from probability density of u:
-			# logp_pi_a = (dist.log_prob(u) - torch.log(1 - a.pow(2) + 1e-6)).sum(dim=1, keepdim=True)
-			# Derive from the above equation. No a, thus no tanh(h), thus less gradient vanish and more stable.
-			logp_pi_a = dist.log_prob(u).sum(axis=1, keepdim=True) - (2 * (np.log(2) - u - F.softplus(-2 * u))).sum(axis=1, keepdim=True)
-		else:
-			logp_pi_a = None
-
-		return a, logp_pi_a
-
-class Double_Q_Critic(nn.Module):
-	def __init__(self, state_dim, action_dim, hid_shape):
-		super(Double_Q_Critic, self).__init__()
-		layers = [state_dim + action_dim] + list(hid_shape) + [1]
-
-		self.Q_1 = build_net(layers, nn.ReLU, nn.Identity)
-		self.Q_2 = build_net(layers, nn.ReLU, nn.Identity)
-
-	def forward(self, state, action):
-		sa = torch.cat([state, action], 1)
-		q1 = self.Q_1(sa)
-		q2 = self.Q_2(sa)
-		return q1, q2
-
-#reward engineering for better training
-def Reward_adapter(r, EnvIdex):
-	# For Pendulum-v0
-	if EnvIdex == 0:
-		r = (r + 8) / 8
-	# For LunarLander
-	elif EnvIdex == 1:
-		if r <= -100: r = -10
-	# For BipedalWalker
-	elif EnvIdex == 4 or EnvIdex == 5:
-		if r <= -100: r = -1
-	return r
-
-
-def Action_adapter(a,max_action):
-	#from [-1,1] to [-max,max]
-	return  a*max_action
-
-def Action_adapter_reverse(act,max_action):
-	#from [-max,max] to [-1,1]
-	return  act/max_action
-
-## Discrete SAC
-class Policy_Net(nn.Module):
-	def __init__(self, state_dim, action_dim, hid_shape):
-		super(Policy_Net, self).__init__()
-		layers = [state_dim] + list(hid_shape) + [action_dim]
-		self.P = build_net(layers, nn.ReLU, nn.Identity)
-
-	def forward(self, s):
-		logits = self.P(s)
-		probs = F.softmax(logits, dim=1)
-		return probs
-
-class Double_Q_Net(nn.Module):
-	def __init__(self, state_dim, action_dim, hid_shape):
-		super(Double_Q_Net, self).__init__()
-		layers = [state_dim] + list(hid_shape) + [action_dim]
-
-		self.Q1 = build_net(layers, nn.ReLU, nn.Identity)
-		self.Q2 = build_net(layers, nn.ReLU, nn.Identity)
-
-	def forward(self, s):
-		q1 = self.Q1(s)
-		q2 = self.Q2(s)
-		return q1,q2
-
-class ReplayBuffer(object):
-	def __init__(self, state_dim, action_dim, max_size, device):
-		self.max_size = max_size
-		self.device = device
-		self.ptr = 0
-		self.size = 0
-
-		self.s = torch.zeros((max_size, state_dim) ,dtype=torch.float,device=self.device)
-		self.a = torch.zeros((max_size, action_dim) ,dtype=torch.float,device=self.device)
-		self.r = torch.zeros((max_size, 1) ,dtype=torch.float,device=self.device)
-		self.s_next = torch.zeros((max_size, state_dim) ,dtype=torch.float,device=self.device)
-		self.dw = torch.zeros((max_size, 1) ,dtype=torch.bool,device=self.device)
-
-	def add(self, s, a, r, s_next, dw):
-		#每次只放入一个时刻的数据
-		self.s[self.ptr] = torch.from_numpy(s).to(self.device)
-		self.a[self.ptr] = torch.from_numpy(a).to(self.device) # Note that a is numpy.array
-		self.r[self.ptr] = r
-		self.s_next[self.ptr] = torch.from_numpy(s_next).to(self.device)
-		self.dw[self.ptr] = dw
-
-		self.ptr = (self.ptr + 1) % self.max_size #存满了又重头开始存
-		self.size = min(self.size + 1, self.max_size)
-
-	def sample(self, batch_size):
-		ind = torch.randint(0, self.size, device=self.device, size=(batch_size,))
-		return self.s[ind], self.a[ind], self.r[ind], self.s_next[ind], self.dw[ind]
 
 def evaluate_policy(env, agent, turns = 3):
 	total_scores = 0
@@ -153,6 +24,20 @@ def evaluate_policy(env, agent, turns = 3):
 			s = s_next
 	return int(total_scores/turns)
 
+def evaluate_policy_PPOD(env, agent, turns = 3):
+    total_scores = 0
+    for j in range(turns):
+        s, info = env.reset()
+        done = False
+        while not done:
+            # Take deterministic actions at test time
+            a, logprob_a = agent.select_action(s, deterministic=True)
+            s_next, r, dw, tr, info = env.step(a)
+            done = (dw or tr)
+
+            total_scores += r
+            s = s_next
+    return int(total_scores/turns)
 
 def str2bool(v):
 	'''transfer str to bool for argparse'''
